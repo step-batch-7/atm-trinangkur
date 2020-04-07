96a9b41f707a9981093b33953ce5b3ccefcd94a7
Trinangkur Chatterjee <58026183+trinangkur@users.noreply.github.com>
2020-04-08 04:15:38 +0530

    Added display notes function

--- atm.c
+++ atm.c
@@ -1,6 +1,7 @@
 #include<math.h>
+#include<stdio.h>
 
-unsigned int get_money(int amount) {
+unsigned int get_money(unsigned short int amount) {
   int denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
   unsigned int dispensed_notes = 0x00;
   if(amount > 31999)
@@ -11,4 +12,16 @@ unsigned int get_money(int amount) {
     amount = amount % denominations[i];
   }
   return dispensed_notes;
+}
+
+void display_notes(unsigned int notes) {
+  int denominations[] = {2000, 500, 100, 50, 20, 10, 5, 1};
+  unsigned int notes_count;
+  for (int i = 0; i < 8; i++)
+  {
+    notes_count = notes & (0xf<<((7-i)*4));
+    if (notes_count) {
+      printf("%d note(s) of Rs %d\n",notes_count>>((7-i)*4),denominations[i]);
+    }
+  }
 }
--- main.c
+++ main.c
@@ -6,9 +6,8 @@ int main(void)
   unsigned short int amounts[] = {1, 10, 39, 589, 2590, 31999};
   for (int i = 0; i < 6; i++)
   {
-    unsigned int notes = get_money(amounts[i]);
-    printf("%5u %08x\n", amounts[i], notes);
+    printf("\nNotes for Rs %u:\n", amounts[i]);
+    display_notes(get_money(amounts[i]));
   }
-
   return 0;
 }
